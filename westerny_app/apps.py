b33bcba6a80d51d50e966595c3361c3654e2a857
AndrzejBakula <goldenwroc@gmail.com>
2020-10-31 12:05:50 +0100

    checking

---
+++ westerny_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WesternyAppConfig(AppConfig):
+    name = 'westerny_app'
---
+++ westerny_app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from datetime import timezone, date, timedelta
+from django.views import View
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, "base.html")
