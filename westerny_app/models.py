9c4ec12e5a9d69c91c8fb2bc4f77646ca70e53c6
AndrzejBakula <goldenwroc@gmail.com>
2020-11-01 14:05:29 +0100

    models added

--- westerny_app/models.py
+++ westerny_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from datetime import timezone, date, timedelta
 
-# Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    person_image = models.ImageField(blank=True, null=True, upload_to="person_images/")
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=64)
+    genre_image = models.ImageField(blank=True, null=True, upload_to="genre_images/")
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    director = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='director')
+    screenplay = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='screenplay')
+    starring = models.ManyToManyField(Person, through="PersonMovie")
+    year = models.IntegerField(null=False)
+    rating = models.FloatField(null=True)
+    genre = models.ManyToManyField(Genre)
+    movie_image = models.ImageField(blank=True, null=True, upload_to="movie_images/")
+
+
+class PersonMovie(models.Model):
+    role = models.CharField(max_length=128, null=True)
+    persons = models.ForeignKey(Person, on_delete=models.CASCADE)
+    movies = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
--- westerny_app/views.py
+++ westerny_app/views.py
@@ -8,3 +8,8 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, "index.html")
+
+
+# class MoviesView(View):
+#     def get(self, request):
+#         movies = Movie.objects.all()
