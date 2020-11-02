4d08c0eebeeab9850921f5473e78fe354893fbd8
AndrzejBakula <goldenwroc@gmail.com>
2020-11-02 11:59:59 +0100

    who_added added

--- westerny_app/models.py
+++ westerny_app/models.py
@@ -5,11 +5,13 @@ class Person(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     person_image = models.ImageField(blank=True, null=True, upload_to="person_images/")
+    who_added = models.CharField(max_length=64, default="Westerny")
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=64)
     genre_image = models.ImageField(blank=True, null=True, upload_to="genre_images/")
+    who_added = models.CharField(max_length=64, default="Westerny")
 
 
 class Movie(models.Model):
@@ -21,6 +23,7 @@ class Movie(models.Model):
     rating = models.FloatField(null=True)
     genre = models.ManyToManyField(Genre)
     movie_image = models.ImageField(blank=True, null=True, upload_to="movie_images/")
+    who_added = models.CharField(max_length=64, default="Westerny")
 
 
 class PersonMovie(models.Model):
